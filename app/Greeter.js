36a5e8531a1be4c85ccc62e1f2ed9a53c4821a21
pealpool <pealpool@gmail.com>
2019-11-19 12:02:24 +0800

    webpack add

---
+++ app/Greeter.js
@@ -0,0 +1,8 @@
+"use strict";
+
+
+module.exports = function() {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greetings!";
+    return greet;
+};
---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+
+
+const greeter = require('./Greeter.js');
+document.querySelector("#root").appendChild(greeter());
