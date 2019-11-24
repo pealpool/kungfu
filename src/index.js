6a22cf46a289d04232c9961c50367a175f55be59
pealpool <pealpool@gmail.com>
2019-11-24 19:55:13 +0800

    ++start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import $ from 'jquery'
 import './css/baseCss.scss'
 
-$(function () {
-    $('li:odd').css('backgroundColor', 'pink')
-    $('li:even').css('backgroundColor', 'lightblue')
-})
-
-class Person {
-    static info = 'aaa'
-}
-
-console.log(Person.info)
